d4bf85d4691142362ad459ad852b5dfa15d5843e
levuongthinh-tech <levuongthinh@gmail.com>
2026-06-23 03:28:02 +0700

    Fix Footer: /xe-oto → /o-to links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -14,19 +14,19 @@ export default function Footer() {
         <div>
           <h4 className="font-semibold text-white mb-3">Ô tô</h4>
           <ul className="space-y-2">
-            <li><Link href="/xe-oto" className="hover:text-white transition">Tất cả ô tô</Link></li>
-            <li><Link href="/xe-oto?segment=Sedan" className="hover:text-white transition">Sedan</Link></li>
-            <li><Link href="/xe-oto?segment=SUV" className="hover:text-white transition">SUV</Link></li>
-            <li><Link href="/xe-oto?segment=MPV" className="hover:text-white transition">MPV</Link></li>
+            <li><Link href="/o-to" className="hover:text-white transition">Tất cả ô tô</Link></li>
+            <li><Link href="/o-to?type=sedan" className="hover:text-white transition">Sedan</Link></li>
+            <li><Link href="/o-to?type=suv" className="hover:text-white transition">SUV</Link></li>
+            <li><Link href="/o-to?type=mpv" className="hover:text-white transition">MPV</Link></li>
           </ul>
         </div>
         <div>
           <h4 className="font-semibold text-white mb-3">Xe máy</h4>
           <ul className="space-y-2">
             <li><Link href="/xe-may" className="hover:text-white transition">Tất cả xe máy</Link></li>
-            <li><Link href="/xe-may?brand=honda" className="hover:text-white transition">Xe ga</Link></li>
-            <li><Link href="/xe-may?brand=yamaha" className="hover:text-white transition">Sportbike</Link></li>
-            <li><Link href="/xe-may?brand=vinfast" className="hover:text-white transition">Xe điện</Link></li>
+            <li><Link href="/xe-may?type=xe-ga" className="hover:text-white transition">Xe ga</Link></li>
+            <li><Link href="/xe-may?type=sportbike" className="hover:text-white transition">Sportbike</Link></li>
+            <li><Link href="/xe-may?type=xe-dien" className="hover:text-white transition">Xe điện</Link></li>
           </ul>
         </div>
         <div>
