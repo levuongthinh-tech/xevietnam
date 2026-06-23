643237d616dfbcb8d8717fbdb04446063de897fe
levuongthinh-tech <levuongthinh@gmail.com>
2026-06-23 09:15:24 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**.vnexpress.net' },
+      { protocol: 'https', hostname: '**.vnecdn.net' },
       { protocol: 'https', hostname: '**.supabase.co' },
     ],
   },
